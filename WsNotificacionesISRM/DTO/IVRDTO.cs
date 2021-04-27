da5e8280a7ff195896692eb387630a47f4bed7fb
lrosonda <63877704+lrosonda@users.noreply.github.com>
2021-04-27 16:46:39 -0500

    1 Commit. WSDL IVR

---
+++ WsNotificacionesISRM/DTO/IVRDTO.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WsNotificacionesISRM.DTO
+{
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://ensa.com.pa/")]
+    public partial class SolicitudEnvioIVR
+    {
+        private String _tipo;
+
+        public string tipo { get => _tipo; set => _tipo = value; }
+    }
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://ensa.com.pa/")]
+    public partial class RespuestaEnvioIVR
+    {
+        private Int32 _identificadorMensaje;
+        private String _tipo;
+        private String _nombre;
+        private String _segundoNombre;
+        private String _apellido;
+        private String _segundoApellido;
+        private String _telefonoResidencial;
+        private String _telefonoMovil;
+        private String _areaAfectada;
+        private String _estadoTrabajo;
+        private String  _fechaDeCorte;
+        private String _fechaDeRestauracion;
+        private String _mensajeRespuesta;
+        private Int32 _codigoMensaje;
+        public int identificadorMensaje { get => _identificadorMensaje; set => _identificadorMensaje = value; }
+        public string tipo { get => _tipo; set => _tipo = value; }
+        public string nombre { get => _nombre; set => _nombre = value; }
+        public string segundoNombre { get => _segundoNombre; set => _segundoNombre = value; }
+        public string apellido { get => _apellido; set => _apellido = value; }
+        public string segundoApellido { get => _segundoApellido; set => _segundoApellido = value; }
+        public string telefonoResidencial { get => _telefonoResidencial; set => _telefonoResidencial = value; }
+        public string telefonoMovil { get => _telefonoMovil; set => _telefonoMovil = value; }
+        public string areaAfectada { get => _areaAfectada; set => _areaAfectada = value; }
+        public string estadoTrabajo { get => _estadoTrabajo; set => _estadoTrabajo = value; }
+        public string fechaDeCorte { get => _fechaDeCorte; set => _fechaDeCorte = value; }
+        public string fechaDeRestauracion { get => _fechaDeRestauracion; set => _fechaDeRestauracion = value; }
+        public string mensajeRespuesta { get => _mensajeRespuesta; set => _mensajeRespuesta = value; }
+        public int codigoMensaje { get => _codigoMensaje; set => _codigoMensaje = value; }
+    }
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://ensa.com.pa/")]
+    public partial class SolitudConfirmacionIVR {
+        private Int32 _identificadorMensaje;
+        private String _fechaRegistro;
+        private String _estadoLLamada;
+        public int identificadorMensaje { get => _identificadorMensaje; set => _identificadorMensaje = value; }
+        public string fechaRegistro { get => _fechaRegistro; set => _fechaRegistro = value; }
+        public string estadoLLamada { get => _estadoLLamada; set => _estadoLLamada = value; }
+    }
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://ensa.com.pa/")]
+    public partial class RespuestaConfirmacionIVR {
+        private String _mensajeRespuesta;
+        private Int32 _codigoMensaje;
+        public string mensajeRespuesta { get => _mensajeRespuesta; set => _mensajeRespuesta = value; }
+        public int codigoMensaje { get => _codigoMensaje; set => _codigoMensaje = value; }
+    }
+
+}
--- WsNotificacionesISRM/WebServiceIVR.asmx.cs
+++ WsNotificacionesISRM/WebServiceIVR.asmx.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using WsNotificacionesISRM.DTO;
 
 namespace WsNotificacionesISRM
 {
     /// <summary>
     /// Descripción breve de WebServiceIVR
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://ensa.com.pa/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
@@ -18,9 +19,15 @@ namespace WsNotificacionesISRM
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public RespuestaEnvioIVR envioIVR(SolicitudEnvioIVR solicitudEnvioIVR)
         {
-            return "Hola a todos";
+            RespuestaEnvioIVR respuestaEnvioIVR = new RespuestaEnvioIVR();
+            return respuestaEnvioIVR;
+        }
+        [WebMethod]
+        public RespuestaConfirmacionIVR confirmacionIVR(SolitudConfirmacionIVR solitudConfirmacionIVR) {
+            RespuestaConfirmacionIVR respuestaConfirmacionIVR = new RespuestaConfirmacionIVR();
+            return respuestaConfirmacionIVR;
         }
     }
 }
